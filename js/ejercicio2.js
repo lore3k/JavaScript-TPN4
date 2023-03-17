91bb097f952b3e5d01832dc6f1d263ef5d24637f
Lore Ginard <lorenaginard@hotmail.com>
2023-03-17 13:06:21 -0300

    Enunciado ejercicio 2

---
+++ js/ejercicio2.js
@@ -0,0 +1,14 @@
+/*
+Modelando clases
+
+Cuenta bancaria - Dificultad:  🟢🟡
+2-Escribe un programa que cree un objeto "cuenta" con las siguientes propiedades:
+Una propiedad titular con el valor "Alex".
+Una propiedad saldo, teniendo como valor inicial 0.
+Un método ingresar() que permita añadir dinero a la cuenta, pasando la cantidad como parámetro
+Un método extraer() que permita retirar la cantidad pasada como parámetro.
+Un método informar() que retorne la información del estado de la cuenta. 
+
+Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a 
+mostrar la descripción del estado de la cuenta.
+*/
