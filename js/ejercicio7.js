1703671d78f2dc5039296d641acf76af4b4e8230
Lore Ginard <lorenaginard@hotmail.com>
2023-03-17 13:16:48 -0300

    enunciado ejercicio 7

---
+++ js/ejercicio7.js
@@ -0,0 +1,24 @@
+/*
+Agenda telefónica - Dificultad:  🟢🟡🔴
+
+7- Nos piden realizar una agenda telefónica de contactos.
+
+Un contacto está definido por un nombre y un teléfono. Se considera que un contacto es igual a otro 
+cuando sus nombres son iguales.
+
+Una agenda de contactos está formada por un conjunto de contactos. Se podrá crear de dos formas, 
+indicando nosotros el tamaño o con un tamaño por defecto (10).
+
+Los métodos de la agenda serán los siguientes:
+
+aniadirContacto(Contacto): Añade un contacto a la agenda, sino la agenda no puede almacenar más contactos 
+indicar por pantalla.
+existeContacto(Conctacto): indica si el contacto pasado existe o no.
+listarContactos(): Lista toda la agenda
+buscarContacto(nombre): busca un contacto por su nombre y muestra su teléfono.
+eliminarContacto(Contacto c): elimina el contacto de la agenda, indica si se ha eliminado o no por pantalla
+agendaLlena(): indica si la agenda está llena.
+huecosLibres(): indica cuántos contactos más podemos ingresar.
+Crea un menú con opciones por consola para probar todas estas funcionalidades.
+
+*/
