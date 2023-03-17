7ada9ebe7576828d98f8f0d2f2f35b2fee0d9d8f
Lore Ginard <lorenaginard@hotmail.com>
2023-03-17 13:14:20 -0300

    Enunciado ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,17 @@
+/*
+Libros - Dificultad:  🟢🟡🔴
+6- Crear una clase Libro que contenga al menos las siguientes propiedades:
+ISBN
+Título
+Autor
+Número de páginas
+
+Crear sus respectivos métodos get y set correspondientes para cada propiedad. 
+Crear el método mostrarLibro() para mostrar la información relativa al libro con 
+el siguiente formato:
+
+“El libro xxx con ISBN xxx creado por el autor xxx tiene páginas xxx”
+
+Crear al menos 2 objetos libros y utilizar el método mostrarLibro();
+Por último, indicar cuál de los 2 objetos “libros” tiene más páginas.
+*/
