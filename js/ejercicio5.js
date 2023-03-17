24402a1d8999c37266171a6ff251dc6e91679e53
Lore Ginard <lorenaginard@hotmail.com>
2023-03-17 13:12:22 -0300

    Enunciado ejercicio 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,17 @@
+/*
+Generaciones - Dificultad:  🟢🟡🔴
+
+5- Crea una clase llamada Persona que siga las siguientes condiciones:
+Sus propiedades son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura, 
+año de nacimiento. Si quieres añadir alguna propiedad extra puedes hacerlo.
+Los métodos que se debe poder utilizar  son:
+mostrarGeneracion: este método debe mostrar un mensaje indicando a qué generación 
+pertenece la persona creada y cual es el rasgo característico de esta generación.
+Para realizar este método tener en cuenta la siguiente tabla de generaciones:
+
+esMayorDeEdad: indica si es mayor de edad, devuelve un mensaje indicando que 
+la persona es mayor de edad.
+mostrarDatos: devuelve toda la información del objeto.
+generaDNI(): genera un número aleatorio de 8 cifras.
+
+ */
