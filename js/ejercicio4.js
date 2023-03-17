06a63566255497a4c2035d8fd65579e0004147a6
Lore Ginard <lorenaginard@hotmail.com>
2023-03-17 13:08:34 -0300

    Enunciado ejercicio 4

---
+++ js/ejercicio4.js
@@ -0,0 +1,10 @@
+/*
+Producto - Dificultad:  🟢🟡
+4- Escribe una clase Producto para crear objetos. Estos objetos, deben presentar 
+las propiedades código, nombre y precio, además del método imprime datos, 
+el cual escribe por pantalla los valores de sus propiedades.
+Posteriormente, cree tres instancias de este objeto y guárdalas en un array.
+Por último, utilice el método imprime datos para mostrar por pantalla los valores de 
+los tres objetos instanciados.
+
+*/
