e25de5bf92945f9cc4b7496db9340a81f757c612
Lore Ginard <lorenaginard@hotmail.com>
2023-03-17 13:03:27 -0300

    Enunciado ejercicio 1

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,3 +1,7 @@
 /*
-
+Crear objetos
+Dificultad:  🟢
+1- Crea un objeto llamado auto que tenga algunas características 
+como el color, marca, modelo y si está encendido o apagado. 
+Crea los métodos necesarios para permitir encender y apagar el auto.
  */
