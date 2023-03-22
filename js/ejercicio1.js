cd8f4f7141f72888643aa8db440329c200c19e35
Lore Ginard <lorenaginard@hotmail.com>
2023-03-22 19:30:59 -0300

    Ejercicio 1 finalizado

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -5,3 +5,25 @@ Dificultad:  🟢
 como el color, marca, modelo y si está encendido o apagado. 
 Crea los métodos necesarios para permitir encender y apagar el auto.
  */
+
+
+let auto = { 
+    id: 125,    
+    color: "Azul",  
+    marca: "Ford" ,
+    modelo: "Fiesta",
+    encendido: false,   
+    
+    encender: function (){
+        auto.encender = true;
+        document.write(`<p>El motor está encendido</p>`);
+    },
+    apagar: function(){ document.write(`<p>El motor está apagado</p>`);
+        auto.encender = false;
+    }
+}
+
+auto.encender();
+auto.apagar();
+
+document.write("Color: "+auto.color+`<br> Marca: `+auto.marca+`<br> Modelo: `+auto.modelo+`<br> Encendido?: `+auto.encendido);
