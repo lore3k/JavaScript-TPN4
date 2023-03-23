6f0524e75f50bf48c74fe133e9306c33e5ee3166
Lore Ginard <lorenaginard@hotmail.com>
2023-03-22 22:00:45 -0300

    Ejercicio 3 terminado

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -6,3 +6,38 @@ con las propiedades de alto y ancho, mas los métodos necesarios para modificar 
 mostrar sus propiedades, calcular el perímetro y el área
 
 */
+
+class Rectangulo{
+    constructor(alto,ancho){
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+
+    mostrar(){
+        document.write("Alto: "+this.alto+`<br>`);
+        document.write("Ancho: "+this.ancho+`<br>`);
+    }
+
+    perimetro(){
+        let per = 2*(this.alto+this.ancho);
+        document.write("El perimetro es: "+per);
+    }
+
+    area(){
+        let ar = this.alto*this.ancho;
+        document.write("El área es: "+ar);
+    }
+}
+
+const rectangulo1 = new Rectangulo(3,3);
+const rectangulo2 = new Rectangulo(4,5);
+
+document.write("<h5>Rectángulo 1</h5>");
+rectangulo1.mostrar();
+document.write("<h5>Rectángulo 2</h5>");
+rectangulo2.mostrar();
+
+document.write("<h5>Rectángulo 1</h5>");
+rectangulo1.perimetro();
+document.write("<h5>Rectángulo 2</h5>");
+rectangulo2.area();
