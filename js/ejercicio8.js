6d6fe1a5fb79beb2575abf5a2063f0fef956d4b8
Lore Ginard <lorenaginard@hotmail.com>
2023-06-14 17:08:39 -0300

    terminado

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -2,3 +2,54 @@
 "profesión", y los métodos "saludar" y "despedirse". Luego, crea dos objetos de la
 clase "Persona" con diferentes valores para sus propiedades y llama a sus métodos
 "saludar" y "despedirse". */
+
+
+class Persona {
+    #nombre;
+    #anios;
+    #profesion;
+    constructor(nombre,edad,profesion){
+        this.#nombre=nombre;
+        this.#anios=edad;
+        this.#profesion=profesion;
+    }
+
+    get nombre(){
+        return this.#nombre;
+    }
+    set nombre(nuevoNombre){
+        this.#nombre=nuevoNombre;
+    }
+
+    get edad(){
+        return this.#anios;
+    }
+    set edad(nuevaEdad){
+        if(edad>0){
+            this.#anios=nuevaEdad;
+        }else{
+            document.write("Ingrese un número válido");
+        }
+    }
+
+    get profesion(){
+        return this.#profesion;
+    }
+    set profesion(nuevaProfesion){
+        this.#profesion=nuevaProfesion;
+    }
+
+    saludar(){
+        document.write(this.#nombre + " dice HOLA");
+    }
+    despedir(){
+        document.write(this.#nombre + " dice ADIÓS");
+    }
+}
+
+const persona1 = new Persona("Juan",35,"Actor");
+const persona2 = new Persona("Carla",18,"Estudiante");
+
+
+persona1.saludar();
+persona2.despedir();
